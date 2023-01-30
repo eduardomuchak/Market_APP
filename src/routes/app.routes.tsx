7a0d636dd86e8c4d5d218b273ee2fb5433d48b4d
Eduardo Muchak <eduardomuchak@gmail.com>
2023-01-30 19:19:39 -0300

    style: :art: estilizacao bottom tabs

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,14 +3,50 @@ import { Home } from '../screens/Home';
 import { Categories } from '../screens/Categories';
 import { List } from '../screens/List';
 import { Register } from '../screens/Register';
+import { TouchableOpacity, View, Dimensions } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { House, Package, ListChecks, Plus } from 'phosphor-react-native';
 
 const BottomTab = createBottomTabNavigator();
 
 export function AppRoutes() {
+  const { navigate } = useNavigation();
+  const { width } = Dimensions.get('window');
+
   return (
     <BottomTab.Navigator
       screenOptions={{
         headerShown: false,
+      }}
+      tabBar={() => {
+        return (
+          <View className="flex flex-row justify-between items-center bg-white w-full h-20 px-6">
+            <TouchableOpacity
+              className={`width-${width / 4} p-7`}
+              onPress={() => navigate('home')}
+            >
+              <House size={24} color="#f50057" weight="fill" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              className={`width-${width / 4}  p-7`}
+              onPress={() => navigate('categories')}
+            >
+              <Package size={24} color="#f50057" weight="fill" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              className={`width-${width / 4}  p-7`}
+              onPress={() => navigate('list')}
+            >
+              <ListChecks size={24} color="#f50057" weight="fill" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              className={`width-${width / 4}  p-7`}
+              onPress={() => navigate('register')}
+            >
+              <Plus size={24} color="#f50057" weight="fill" />
+            </TouchableOpacity>
+          </View>
+        );
       }}
     >
       <BottomTab.Screen name="home" component={Home} />
