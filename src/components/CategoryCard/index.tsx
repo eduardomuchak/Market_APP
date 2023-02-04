ede169a60ff13280732ce4b685b96e54b6c3d106
Eduardo Muchak <eduardomuchak@gmail.com>
2023-02-04 20:58:27 -0300

    feat: :sparkles: integracao backend get all categories

--- src/components/CategoryCard/index.tsx
+++ src/components/CategoryCard/index.tsx
@@ -1,10 +1,20 @@
-import { Text, TouchableOpacity, View } from 'react-native';
+import {
+  Text,
+  TouchableOpacity,
+  View,
+  TouchableOpacityProps,
+} from 'react-native';
 
-export function CategoryCard({ categoryName }: { categoryName: string }) {
+interface Props extends TouchableOpacityProps {
+  categoryName: string;
+}
+
+export function CategoryCard({ categoryName, ...rest }: Props) {
   return (
     <TouchableOpacity
       className="bg-white rounded-md flex flex-col items-center justify-center w-[120px] h-[120px] mx-1 mb-2"
       activeOpacity={0.7}
+      {...rest}
     >
       <View className="bg-category1 rounded-full w-[66px] h-[66px]" />
       <Text className="font-poppinsMedium text-xs text-labelText mt-3">
